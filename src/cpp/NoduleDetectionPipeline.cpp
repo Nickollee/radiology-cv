c3f0e441d911e9202e5dc7bb2d15ad4dd0383af1
Brian Vanover <brvanove@microsoft.com>
2017-01-18 19:39:35 -0800

    test metadata loader

--- src/cpp/NoduleDetectionPipeline.cpp
+++ src/cpp/NoduleDetectionPipeline.cpp
@@ -1,4 +1,7 @@
 #include "NoduleDetectionPipeline.hpp"
+#include "CSVReader.hpp"
+
+#include <iostream>
 
 NoduleDetectionPipeline::NoduleDetectionPipeline(std::string metadataFileName)
 {
@@ -7,104 +10,30 @@ NoduleDetectionPipeline::NoduleDetectionPipeline(std::string metadataFileName)
 
 void NoduleDetectionPipeline::ReadInMetadata()
 {
-    std::ifstream ifs;
-    ifs.open (_metadataFileName.c_str(), std::ifstream::in);
-    std::string value;
-    std::list<std::string> values;
-    std::getline ( ifs, value, '\n' );
+    CSVReader reader(_metadataFileName, true);
+    std::vector<std::vector<std::string>> records = reader.Read(',');
 
-    std::vector< std::vector<std::string> > result;
-    while (!ifs.eof())
+    for (int i = 0; i < records.size(); i++)
     {
-        //go through every line
-        std::string line;
-        std::vector<std::string> tmp;
-        size_t pos=std::string::npos;
-        getline(ifs,line, '\n');
-        std::cout << line << "\n";
-        //loop through the ,
-        while ((pos=line.find_first_of(','))!=std::string::npos)
-        {
-            //extract the component sans ,
-            tmp.push_back(line.substr(0,pos-1));
-            //erase the val including ,
-            line.erase(0,pos);
-        }
-        result.push_back(tmp);
-    }
-
-    // while ( ifs.good() )
-    // {
-    //     std::getline ( ifs, value, '\n' ); // read a string until next comma: http://www.cplusplus.com/reference/string/getline/
-    //     if (value.find(',') != std::string::npos) {
-    //         split_line(value, ",", values);
-    //     } else {
-    //         values.push_back(value);
-    //     }
-    // }
-
-    // std::list<std::string>::const_iterator it = values.begin();
-
-    for (int j = 0; j < result.size(); j++)
-    {
-        std::vector<std::string> resultJ;
-        resultJ = result[j];
-        for (int i = 0; i < resultJ.size(); i++)
-        {
-            std::string fileName;
-            int subtlety;
-            int size;
-            int age;
-            bool isMale;
-            int x;
-            int y;
-            bool isMalignant;
-            bool hasNodule;
-            std::cout << "Hello: " << resultJ[i] << "\n";
-
-            switch (i)
-            {
-                case 0:
-                    fileName = resultJ[i];
-                    break;
-                case 1:
-                    subtlety = (int)(std::strtol(resultJ[i].c_str(), NULL, 10));
-                    break;
-                case 2:
-                    size = (int)(std::strtol(resultJ[i].c_str(), NULL, 10));
-                    break;
-                case 3:
-                    age = (int)(std::strtol(resultJ[i].c_str(), NULL, 10));
-                    break;
-                case 4:
-                    isMale = resultJ[i].compare("TRUE");
-                    break;
-                case 5:
-                    x = (int)(std::strtol(resultJ[i].c_str(), NULL, 10));
-                    break;
-                case 6:
-                    y = (int)(std::strtol(resultJ[i].c_str(), NULL, 10));
-                    break;
-                case 7:
-                    isMalignant = resultJ[i].compare("TRUE");
-                    break;
-                case 8:
-                    hasNodule = resultJ[i].compare("TRUE");
-                    break;
-                default:
-                    break;
-            }
-
-            Radiograph xray(fileName, subtlety, size, age, isMale, x, y, isMalignant, hasNodule);
-            std::cout << xray << "\n";
-            _xrays.push_back(xray);
-        }
+        std::vector<std::string> record = records[i];
+        std::string filename = record[0];
+        int subtlety = std::stoi(record[1]);
+        int size = std::stoi(record[2]);
+        int age = std::stoi(record[3]);
+        bool isMale = record[4] == "TRUE";
+        int x = std::stoi(record[5]);
+        int y = std::stoi(record[6]);
+        bool isMalignant = std::stoi(record[7]);
+        bool hasNodule = std::stoi(record[8]);
+        Radiograph xray(fileName, subtlety, size, age, isMale, x, y, isMalignant, hasNodule);
+        std::cout << xray << "\n";
+        _xrays.push_back(xray);        
     }
 }
 
 void NoduleDetectionPipeline::PrintMetadata()
 {
-    for (std::list<Radiograph>::const_iterator iterator = _xrays.begin(), end = _xrays.end(); iterator != end; ++iterator) {
+    for (std::vector<Radiograph>::const_iterator iterator = _xrays.begin(), end = _xrays.end(); iterator != end; ++iterator) {
         std::cout << *iterator << "\n";
     }
 }
--- src/cpp/NoduleDetectionPipeline.hpp
+++ src/cpp/NoduleDetectionPipeline.hpp
@@ -1,28 +1,15 @@
 #include "Radiograph.hpp"
-#include <iostream>
-#include <fstream>
+#include "CSVReader.hpp"
+
 #include <string>
-#include <cstdlib>
-#include <list>
+#include <vector>
+
 
 class NoduleDetectionPipeline
 {
     private:
         std::string _metadataFileName;
-        std::list<Radiograph> _xrays;
-        void split_line(std::string& line, std::string delim, std::list<std::string>& values)
-        {
-            size_t pos = 0;
-            while ((pos = line.find(delim, (pos + 1))) != std::string::npos) {
-                std::string p = line.substr(0, pos);
-                values.push_back(p);
-                line = line.substr(pos + 1);
-            }
-
-            if (!line.empty()) {
-                values.push_back(line);
-            }
-        }
+        std::vector<Radiograph> _xrays;
     public:
         NoduleDetectionPipeline(std::string metadataFileName);
         std::string getMetadataFileName(){return _metadataFileName;}
