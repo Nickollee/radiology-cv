75809f7b6b4ca3841b886ed6533b05351d712b99
Brian Vanover <brvanove@microsoft.com>
2017-01-22 22:37:20 -0800

    fixing opencv train bugs 9

--- src/cpp/NoduleDetectionPipeline.cpp
+++ src/cpp/NoduleDetectionPipeline.cpp
@@ -149,7 +149,7 @@ void NoduleDetectionPipeline::Prepare(std::string rootDataDir, std::string relat
     posFile.close();
     negFile.close();
 
-    std::string sysStr1 = "opencv_createsamples -num 150 -vec " + rootDataDir + "pos.vec -info " + rootDataDir + "info.dat";
+    std::string sysStr1 = "opencv_createsamples -num 10 -vec " + rootDataDir + "pos.vec -info " + rootDataDir + "info.dat";
     std::system(sysStr1.c_str());
 
     std::cout << "Data prepared!";
@@ -159,8 +159,8 @@ void NoduleDetectionPipeline::Train(std::string posVectorFile, std::string negFi
 {
     //int numPos = round(_xraysTrain.size() - 1);
     //int numNeg = numPos * 4;
-    int numPos = 20;
-    int numNeg = 50;
+    int numPos = 2;
+    int numNeg = 2;
     std::string sysStr2 = "opencv_traincascade -data haarcascade_nodule_cxr.xml -vec " + posVectorFile + " -bg " + negFile + " -w " + IntToString(15) + " -h " + IntToString(15) + " -numPos " + IntToString(numPos) + " -numNeg " + IntToString(numNeg) + " -precalcValBufSize 1024 -precalcIdxBufSize 1024 -featureType HAAR";
     std::system(sysStr2.c_str());
     std::cout << "Model trained!";
