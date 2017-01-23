7888a0f662577d8bb526a4e859e89b53101fd756
Brian Vanover <brvanove@microsoft.com>
2017-01-22 19:28:52 -0800

    add cout to debug runtime 16

--- src/cpp/NoduleDetectionPipeline.cpp
+++ src/cpp/NoduleDetectionPipeline.cpp
@@ -74,7 +74,7 @@ void NoduleDetectionPipeline::splitTrainTest(double trainSplit, double testSplit
     int numTest = _xrays.size() - numTrain;
 
     // std::unordered_set<int> selected;
-    bool* selected = new bool[_xraysTrain.size()];
+    bool selected[_xrays.size()];
     while (_xraysTrain.size() < numTrain)
     {
         int recordNum;
