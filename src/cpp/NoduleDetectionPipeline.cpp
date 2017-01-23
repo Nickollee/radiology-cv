5ffedeffd0f7a3176a2bd0f7445e1008c79da7a1
Brian Vanover <brvanove@microsoft.com>
2017-01-22 22:31:41 -0800

    fixing opencv train bugs 7

--- src/cpp/NoduleDetectionPipeline.cpp
+++ src/cpp/NoduleDetectionPipeline.cpp
@@ -124,12 +124,12 @@ void NoduleDetectionPipeline::Prepare(std::string rootDataDir, std::string relat
     std::ofstream negFile(negativeFile.c_str(), std::ios_base::out);
     int h = computeMeanNoduleBoxHeight();
 
-    for (int i = 0; i < _xraysTrain.size(); i++)
-    // for (int i = 0; i < _xrays.size(); i++)
+    // for (int i = 0; i < _xraysTrain.size(); i++)
+    for (int i = 0; i < _xrays.size(); i++)
     {
-        Radiograph r = _xraysTrain[i];
+        // Radiograph r = _xraysTrain[i];
+        Radiograph r = _xrays[i];
         std::string fn = r.getFilename();
-        // Radiograph r = _xrays[i];
         if (r.hasNodule())
         {
             if (posFile.is_open())
@@ -149,7 +149,7 @@ void NoduleDetectionPipeline::Prepare(std::string rootDataDir, std::string relat
     posFile.close();
     negFile.close();
 
-    std::string sysStr1 = "opencv_createsamples -num 1000 -vec " + rootDataDir + "pos.vec -info " + rootDataDir + "info.dat";
+    std::string sysStr1 = "opencv_createsamples -num 150 -vec " + rootDataDir + "pos.vec -info " + rootDataDir + "info.dat";
     std::system(sysStr1.c_str());
 
     std::cout << "Data prepared!";
@@ -159,8 +159,8 @@ void NoduleDetectionPipeline::Train(std::string posVectorFile, std::string negFi
 {
     //int numPos = round(_xraysTrain.size() - 1);
     //int numNeg = numPos * 4;
-    int numPos = 50;
-    int numNeg = 110;
+    int numPos = 100;
+    int numNeg = 300;
     std::string sysStr2 = "opencv_traincascade -data haarcascade_nodule_cxr.xml -vec " + posVectorFile + " -bg " + negFile + " -w " + IntToString(15) + " -h " + IntToString(15) + " -numPos " + IntToString(numPos) + " -numNeg " + IntToString(numNeg) + " -precalcValBufSize 1024 -precalcIdxBufSize 1024 -featureType HAAR";
     std::system(sysStr2.c_str());
     std::cout << "Model trained!";
