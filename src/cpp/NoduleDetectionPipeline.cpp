6787302dbf5f841ffe2945b1c7e184ea79282148
Brian Vanover <brvanove@microsoft.com>
2017-01-22 21:35:30 -0800

    r script remove bad records 2

--- src/cpp/NoduleDetectionPipeline.cpp
+++ src/cpp/NoduleDetectionPipeline.cpp
@@ -158,7 +158,7 @@ void NoduleDetectionPipeline::Train(std::string posVectorFile, std::string negFi
 {
     int numPos = round(_xraysTrain.size() - 1);
     int numNeg = numPos * 4;
-    std::string sysStr2 = "opencv_traincascade -data " + "haarcascade_nodule_cxr.xml" + " -vec " + posVectorFile + " -bg " + negFile + " -w " + IntToString(TRAINING_WINDOW_WIDTH) + " -h " + IntToString(TRAINING_WINDOW_HEIGHT) + " -numPos " + IntToString(numPos) + " -numNeg " + IntToString(numNeg) + " -precalcValBufSize 1024 -precalcIdxBufSize 1024 -featureType HAAR";
+    std::string sysStr2 = "opencv_traincascade -data haarcascade_nodule_cxr.xml -vec " + posVectorFile + " -bg " + negFile + " -w " + IntToString(TRAINING_WINDOW_WIDTH) + " -h " + IntToString(TRAINING_WINDOW_HEIGHT) + " -numPos " + IntToString(numPos) + " -numNeg " + IntToString(numNeg) + " -precalcValBufSize 1024 -precalcIdxBufSize 1024 -featureType HAAR";
     std::system(sysStr2.c_str());
     std::cout << "Model trained!";
     return;
